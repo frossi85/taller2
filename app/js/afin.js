f86d9d1e95857b322abd3e08545c8bf86d1cf933
fdegregorio <adri_1985@live.com>
2015-02-09 11:26:45 -0300

    Afin con autoevaluación listo

--- app/js/afin.js
+++ app/js/afin.js
@@ -1,6 +1,14 @@
 define(['angular', 'angular-route'], function(angular) {
 	var app = angular.module('afin', []);
 
+	app.factory('Data', function () {
+		this.keyA;
+		this.keyB;
+		this.plaintext;
+		
+  		return this;
+	});
+
 	app.directive('ngConfirmClick', [
 	  function(){
 	    return {
@@ -30,7 +38,7 @@ define(['angular', 'angular-route'], function(angular) {
 	});
 
 
-	app.controller('AfinEncryptCtrl', function($scope, $location) {
+	app.controller('AfinEncryptCtrl', function($scope, $location, Data) {
 		this.z26 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 		this._withAutoEvaluation = false;
 		this.start = false;
@@ -45,7 +53,18 @@ define(['angular', 'angular-route'], function(angular) {
 		$scope.result = '';
 		$scope.hideParcial = true;
 		$scope.hideResult = true;
+		$scope.descifrar = false;
 
+		$scope.keyA = Data.keyA;
+		$scope.keyB = Data.keyB;
+		$scope.plaintext = Data.plaintext;
+		$scope.parcialError = false;
+
+		$scope.total = 0;
+		$scope.totalError = 0;
+		$scope.totalOk = 0;
+
+		this._withAutoEvaluation = Data._withAutoEvaluation
 
 		this.init = function() {
 
@@ -53,14 +72,25 @@ define(['angular', 'angular-route'], function(angular) {
 			$scope.result = '';
 			$scope.hideParcial = false;
 			$scope.hideResult = false;
-			
+			$scope.descifrar = false;
+			$scope.parcialError = false;
+		
 			this.start = true;
 			this.step = 0;
 			this.end = false;
 			this.char = '';
 			this.M = 0;
 
-			this.stepCifrado();
+			if(this._withAutoEvaluation == false){
+				this.stepCifrado();
+			}
+		}
+
+		this.validateKey = function(l1,l2) {
+			if(l2 == 0){
+     				return l1;
+   			}
+  			return this.validateKey(l2, l1%l2);
 		}
 
 		this.auto = function(auto) {
@@ -76,14 +106,34 @@ define(['angular', 'angular-route'], function(angular) {
 			$scope.result = '';
 			$scope.hideParcial = true;
 			$scope.hideResult = true;
+			$scope.descifrar = false;
+			$scope.parcialError = false;
+			$scope.total = 0;
+			$scope.totalError = 0;
+			$scope.totalOk = 0;
 		}
 
 		this.next = function() {
 
-			this.stepCifrado();
+			if(this._withAutoEvaluation == false){
+				this.stepCifrado();
+			}
+			else{
+				this.stepCifradoEval();
+			}
 		}
 
 		this.finish = function() {
+
+			if(this._withAutoEvaluation == false){
+				this.finishStep();
+			}
+			else{
+				this.finishStepEval();
+			}			
+		}
+
+		this.finishStep = function(){
 			var result = '';
 
 			for(j = this.step; j < $scope.plaintext.length; j++ ){
@@ -111,9 +161,52 @@ define(['angular', 'angular-route'], function(angular) {
 
 			$scope.result += result;
 			$scope.hideParcial = true;
+			$scope.descifrar = true;
 
 			this.start = false;
 			this.end = true;
+
+		}
+
+		this.finishStepEval = function(){
+			var result = '';
+
+			for(j = this.step; j < $scope.plaintext.length; j++ ){
+	
+				this.char = $scope.plaintext.charAt(j);			
+				var A= parseInt($scope.keyA);
+				var B= parseInt($scope.keyB);
+
+				if(this.char != " "){
+					M = this.getPos(this.char);
+					parcial = this.z26[((( M * A ) + B) % 26 + 26)%26];
+				}
+				else{
+					parcial = " ";
+				}
+
+				if(result != ''){
+					result += parcial;
+									
+				}
+				else{
+					result = parcial;
+				}				
+			}
+
+			if($scope.parcial != result || $scope.parcial == ''){
+				$scope.parcialError = true;
+			}
+			else{
+				$scope.parcialError = false;
+				$scope.result += result;
+				$scope.hideParcial = true;
+				$scope.descifrar = true;
+
+				this.start = false;
+				this.end = true;
+			}
+
 		}
 
 		this.getPos = function(caracter){
@@ -154,9 +247,69 @@ define(['angular', 'angular-route'], function(angular) {
 			}
 		}
 
+		this.stepCifradoEval = function(){
+
+			this.char = $scope.plaintext.charAt(this.step);			
+			var A= parseInt($scope.keyA);
+			var B= parseInt($scope.keyB);
+
+			var parcialCheck = '';
+			$scope.total++;
+
+			if(this.char != " "){
+				M = this.getPos(this.char);
+				parcialCheck = this.z26[((( M * A ) + B) % 26 + 26)%26];
+			}
+			else{
+				parcialCheck = " ";
+			}
+
+			if($scope.parcial != parcialCheck){
+				$scope.parcialError = true;
+				$scope.totalError++;
+			}
+			else{
+				$scope.parcialError = false;			
+
+				if($scope.result != ''){
+					$scope.result += parcialCheck;
+									
+				}
+				else{
+					$scope.result = parcialCheck;
+				}
+			
+				this.step++;
+				
+				$scope.parcial = '';
+
+				if(this.step >= $scope.plaintext.length){
+					this.end = true;
+					this.finish();	
+					$scope.hideParcial = true;
+					$scope.totalOk = $scope.total - $scope.totalError;
+				}
+			}
+		}			
+
+		this.descifrar = function(){
+			$scope.plaintext = $scope.result;
+			$scope.parcial = '';
+			$scope.result = '';
+			$scope.ksa = '';
+			$scope.descifrar = false;
+
+
+			Data.keyA = $scope.keyA;
+			Data.keyB = $scope.keyB;
+			Data.plaintext = $scope.plaintext;
+			Data._withAutoEvaluation = this._withAutoEvaluation;
+			$location.url('/afin/decrypt');
+		}
+
 	});
 
-	app.controller('AfinDecryptCtrl', function($scope, $location) {
+	app.controller('AfinDecryptCtrl', function($scope, $location, Data) {
 
 		this.z26 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 		this._withAutoEvaluation = false;
@@ -171,7 +324,20 @@ define(['angular', 'angular-route'], function(angular) {
 		$scope.plaintext = '';
 		$scope.hideParcial = true;
 		$scope.hideResult = true;
+		$scope.cifrar = false;
 
+		$scope.keyA = Data.keyA;
+		$scope.keyB = Data.keyB;
+		$scope.plaintext = Data.plaintext;
+		console.log("inicio " + Data._withAutoEvaluation);
+		
+		$scope.parcialError = false;
+
+		$scope.total = 0;
+		$scope.totalError = 0;
+		$scope.totalOk = 0;
+
+		this._withAutoEvaluation = Data._withAutoEvaluation
 
 		this.init = function() {
 
@@ -179,19 +345,23 @@ define(['angular', 'angular-route'], function(angular) {
 			$scope.result = '';
 			$scope.hideParcial = false;
 			$scope.hideResult = false;
-			
-			this.invKeyA = this.inversoMulti($scope.keyA);
-
+			$scope.cifrar = false;
+			$scope.parcialError = false;
+		
 			this.start = true;
 			this.step = 0;
 			this.end = false;
 			this.char = '';
 			this.M = 0;
 
-			this.stepDeCifrado();
+			if(this._withAutoEvaluation == false){
+				this.stepDeCifrado();
+			}
 		}
 
 		this.auto = function(auto) {
+
+					console.log("inicio " + Data._withAutoEvaluation);
 			this._withAutoEvaluation = auto;
 			this.reset();
 		}
@@ -204,14 +374,34 @@ define(['angular', 'angular-route'], function(angular) {
 			$scope.result = '';
 			$scope.hideParcial = true;
 			$scope.hideResult = true;
+			$scope.cifrar = false;
+			$scope.parcialError = false;
+			$scope.total = 0;
+			$scope.totalError = 0;
+			$scope.totalOk = 0;
 		}
 
 		this.next = function() {
 
-			this.stepDeCifrado();
+			if(this._withAutoEvaluation == false){
+				this.stepDeCifrado();
+			}
+			else{
+				this.stepDeCifradoEval();
+			}
 		}
 
 		this.finish = function() {
+
+			if(this._withAutoEvaluation == false){
+				this.finishStep();
+			}
+			else{
+				this.finishStepEval();
+			}			
+		}
+
+		this.finishStep = function() {
 			var result = '';
 
 			for(j = this.step; j < $scope.plaintext.length; j++ ){
@@ -239,10 +429,52 @@ define(['angular', 'angular-route'], function(angular) {
 
 			$scope.result += result;
 			$scope.hideParcial = true;
+			$scope.cifrar = true;
 
 			this.start = false;
 			this.end = true;
 		}
+
+		this.finishStepEval = function() {
+			var result = '';
+
+			for(j = this.step; j < $scope.plaintext.length; j++ ){
+	
+				this.char = $scope.plaintext.charAt(j);			
+				var A= parseInt($scope.keyA);
+				var B= parseInt($scope.keyB);
+
+				if(this.char != " "){
+					M = this.getPos(this.char);
+					parcial = this.z26[((( M - B ) * this.invKeyA ) % 26 + 26) % 26];
+				}
+				else{
+					parcial = " ";
+				}
+
+				if(result != ''){
+					result += parcial;
+									
+				}
+				else{
+					result = parcial;
+				}				
+			}
+
+			if($scope.parcial != result || $scope.parcial == ''){
+				$scope.parcialError = true;
+			}
+			else{
+				$scope.parcialError = false;
+				$scope.result += result;
+				$scope.hideParcial = true;
+				$scope.cifrar = true;
+
+				this.start = false;
+				this.end = true;
+			}
+		}
+
 
 		this.getPos = function(caracter){
 			for(i = 0; i < this.z26.length; i++) {
@@ -253,7 +485,13 @@ define(['angular', 'angular-route'], function(angular) {
 		}
 		
 		this.stepDeCifrado = function(){
+
+			console.log("constante A " + $scope.keyA);
+
 			this.invKeyA = this.inversoMulti($scope.keyA);
+
+			
+			console.log("inversa de A " + this.invKeyA);
 
 			this.char = $scope.plaintext.charAt(this.step);	
 			var B= parseInt($scope.keyB);
@@ -266,7 +504,6 @@ define(['angular', 'angular-route'], function(angular) {
 				$scope.parcial = " ";
 			}
 
-						console.log("inicio2 " + $scope.result);
 			if($scope.result != ''){
 				$scope.result += $scope.parcial;
 									
@@ -284,6 +521,50 @@ define(['angular', 'angular-route'], function(angular) {
 			}
 		}
 
+		this.stepDeCifradoEval = function(){
+
+			var parcialCheck = '';
+			this.invKeyA = this.inversoMulti($scope.keyA);
+
+			this.char = $scope.plaintext.charAt(this.step);	
+			var B= parseInt($scope.keyB);
+
+			if(this.char != " "){
+				M = this.getPos(this.char);
+				parcialCheck = this.z26[((( M - B ) * this.invKeyA ) % 26 + 26) % 26];
+			}
+			else{
+				parcialCheck = " ";
+			}
+
+			if($scope.parcial != parcialCheck){
+				$scope.parcialError = true;
+				$scope.totalError++;
+			}
+			else{
+				$scope.parcialError = false;			
+
+				if($scope.result != ''){
+					$scope.result += parcialCheck;
+									
+				}
+				else{
+					$scope.result = parcialCheck;
+				}
+
+				this.step++;
+		
+				$scope.parcial = '';
+
+				if(this.step >= $scope.plaintext.length){
+					this.end = true;
+					this.finish();
+					$scope.hideParcial = true;
+					$scope.totalOk = $scope.total - $scope.totalError;
+				}
+			}
+		}
+
 		this.inversoMulti = function(keyA){
 			var flag;
 			for (i= 0; i < 26; i++){
@@ -292,6 +573,22 @@ define(['angular', 'angular-route'], function(angular) {
 					return i;
 				}
 			}
+			return 0;
+		}
+
+		this.cifrar = function(){
+			$scope.plaintext = $scope.result;
+			$scope.parcial = '';
+			$scope.result = '';
+			$scope.ksa = '';
+			$scope.descifrar = false;
+
+
+			Data.keyA = $scope.keyA;
+			Data.keyB = $scope.keyB;
+			Data.plaintext = $scope.plaintext;
+			Data._withAutoEvaluation = this._withAutoEvaluation;
+			$location.url('/afin/encrypt');
 		}
 	});
 });
