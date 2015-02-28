b8a1a2d2b7b0f7ff0a23f1fe78acbf05d02bc403
Your Name <you@example.com>
2015-02-28 18:29:39 -0300

    Se agrego configuracion de servidor node JS para que se pueda probar facil en la entrega

---
+++ app/server.js
@@ -0,0 +1,25 @@
+// server.js
+
+// Generate a new instance of express server.
+var express = require('express')
+  , http = require('http');
+
+var app = express();
+
+app.use(express.static(process.cwd() + '/'));
+
+/* This will allow Cozy to run your app smoothly but
+ it won't break other execution environment */
+var port = process.env.PORT || 9250;
+var host = process.env.HOST || "127.0.0.1";
+
+// Starts the server itself
+var server = http.createServer(app).listen(port, host, function() {
+  console.log("Server listening to %s:%d within %s environment",
+              host, port, app.get('env'));
+});
+
+// At the root of your website, we show the index.html page
+app.get('/', function(req, res) {
+  res.sendFile('public/index.html', { root: __dirname });
+});
